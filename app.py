fd3c2656c6ad0313c50344afd9017b3310ad1789
Emanuel Victor <emanuel.saturnino@academico.ifpb.edu.br>
2026-07-01 03:40:05 -0300

    built: Create final backend system

--- app.py
+++ app.py
@@ -3,4 +3,46 @@ app = Flask(__name__)
 
 app.secret_key = "Ukiddingme,bro?"
 
-session["tarefas"] = []
+#3 marias
+def check_list():
+    if "tarefas" not in session:
+        session["tarefas"] = []
+    return session["tarefas"]
+
+def add_tarefa(titulo):
+    lista = check_list()
+    lista.append(titulo)
+    session.modified = True
+
+def remove_tarefa(indice):
+    lista = check_list()
+    lista.pop(indice)
+    session.modified = True
+
+@app.route("/remove/<int:indice>")
+def removing(indice):
+    remove_tarefa(indice)
+    session.modified = True
+    return redirect('/')
+    
+
+@app.route("/redireciona")
+def redirecionando():
+    session.modified = True
+    return redirect("/")
+
+@app.route("/", methods = ["POST", "GET"])
+def render_index():
+    lista = check_list() #Return session["tarefas"]
+    if request.method == "GET":
+        return render_template('index.html', tarefas = lista)
+    if request.method == "POST": #Se for post, peguei o formulário, adiciono tarefas, renderizo sucesso
+
+        #Pego formulário
+        titulo = request.form.get("titulo")
+
+        #Adiciono tarefas
+        add_tarefa(titulo)
+
+        #rederia a sucesso (que volta para o index)
+        return render_template("success.html")
